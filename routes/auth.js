face7c8a804e01247a7ad4652989726903ae723f
phezzy-test <57448657+phezzy-test@users.noreply.github.com>
2019-11-08 19:12:52 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -6,16 +6,22 @@ const dbDepts = require('../middleware/get-db-departments');
 const authenticate = require('../middleware/authenticate_admin');
 const authCtrl = require('../controllers/auth');
 
+const b = (req, res, next) => {
+  console.log("before multer req body ", req.body);
+  next();
+}; 
+
 const test1 = (req, res, next) => {
   console.log("hello world from TEST 1");
   next();
-};              
+}; 
 const test2 = (req, res, next) => {
   console.log("hello world from TEST 2");
+   console.log("after multer req body ", req.body);
   next();
 };
 
-router.post('/create-user', test1, authenticate,  test2);
+router.post('/create-user', test1, b, multer,  test2);
 // router.post('/login', authCtrl.login);
 
 module.exports = router;
