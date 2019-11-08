4b52511b67921c0921ef136299a0c0599aad1da4
phezzy-test <57448657+phezzy-test@users.noreply.github.com>
2019-11-08 02:26:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app.use((req, res, next) => {
 // Connect to database
 db.connect().then(() => {
   console.log('Successfully connected to postgresSQL!');
-}).catch(() => {
-  console.log('Unable to connect to postgresSQL!');
+}).catch((err) => {
+  console.log('Unable to connect to postgresSQL!', err);
 });
 
 // Parse request body
