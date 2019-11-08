d24bb6fc756038b49a81ceffbc29d47c7e336f67
phezzy-test <57448657+phezzy-test@users.noreply.github.com>
2019-11-08 12:00:31 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ const dbUserEmails = require('../middleware/get-db-usersEmail');
 const authenticate = require('../middleware/authenticate');
 const authCtrl = require('../controllers/auth');
 
-router.post('/create-user', multer, dbDepts, dbUserEmails, authCtrl.createUser);
+router.post('/create-user', multer, authenticate.admin, dbDepts, dbUserEmails, authCtrl.createUser);
 // router.post('/login', authCtrl.login);
 
 module.exports = router;
