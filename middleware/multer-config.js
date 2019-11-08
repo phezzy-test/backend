dadedf83d2bcfa08a50a592daadfcdec393e3bdb
phezzy-test <57448657+phezzy-test@users.noreply.github.com>
2019-11-08 20:09:55 +0100

    Update multer-config.js

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,28 +1,20 @@
 const multer = require('multer');
 
-console.log('we atleast in multer and req body is : ');
-
-// App accepted mime types
 const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
-  'image/gif': 'gif',
+  'image/png': 'png'
 };
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    if (MIME_TYPES[file.mimetype]) {
-      callback(null, 'uploads');
-    } else {
-      callback({ status: 415, message: 'unsupported file type' }, false);
-    }
+    callback(null, 'images');
   },
   filename: (req, file, callback) => {
     const name = file.originalname.split(' ').join('_');
-    const extension = MIME_TYPES[file.mimetype] ? MIME_TYPES[file.mimetype] : '';
-    callback(null, `${name + Date.now()}.${extension}`);
-  },
+    const extension = MIME_TYPES[file.mimetype];
+    callback(null, name + Date.now() + '.' + extension);
+  }
 });
-const storageConfig = multer({ storage }).any();
-console.log('in side multer and multer return is : ', storageConfig);
-module.exports = storageConfig;;
+
+module.exports = multer({storage: storage}).single('image');
