78165035dd9bd37c8d345b66989b3267c61020bc
phezzy-test <57448657+phezzy-test@users.noreply.github.com>
2019-11-08 21:22:28 +0100

    Update multer-config.js

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -4,13 +4,13 @@ const multer = require('multer');
 const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
-  'image/gif': 'gif',
+  'image/gif': 'gif'
 };
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     if (MIME_TYPES[file.mimetype]) {
-      callback(null, './uploads/');
+      callback(null, './uploads');
     } else {
       callback({ status: 415, message: 'unsupported file type' }, console.log('the bad one.....................................................'));
     }
